76f84f48a19cf3ed736a5566ee2fde57c988adc8
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 15:10:30 +0200

    Fixed Betty requirements

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int first;
-  for (first = 48; first <= 57; first++)
-    putchar (first);
-  putchar ('\n');
-  return (0);
+	int first;
+
+	for (first = 48; first <= 57; first++)
+		putchar(first);
+	putchar('\n');
+	return (0);
 }
