6e2432b01922f2c7186d9f65ad84019b7662c564
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 16:15:45 +0200

    Fixed : Betty and formated output

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+/**
+ * main - display numbers in 00....99
+ * Return: Always  0 (success)
+ */
 int main(void)
 {
-  int first_num = 47;
-  int first_digit;
+	int first_dig;
+	int second_dig;
 
-  for (first_num = 48;first_num <= 57; first_num++)
-    {
-
-    for	(first_digit = 48; first_digit <= 57; first_digit++)
-      {
-        putchar (first_num);
-        putchar(first_digit);
-	putchar	(32);
-        putchar	(44);
-      }	
-    }
-  putchar ('\n');
-  return (0);
+	for (first_dig = 48; first_dig <= 57; first_dig++)
+	{
+		for (second_dig = 48; second_dig <= 57; second_dig++)
+		{
+			putchar(first_dig);
+			putchar(second_dig);
+			if (!(first_dig == 57 && second_dig == 57))
+			{
+				putchar(32);
+				putchar(44);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
