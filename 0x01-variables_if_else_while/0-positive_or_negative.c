c5d0aa37258aaac02c62968b1adf40f39ae7766d
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 12:29:37 +0200

    fixed all of betty errors/warnings FINALLY

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+
 /**
  *main - Entry point
  *Return: Always 0 (Success)
  */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n;
