6927e682c246c0e1838be70ed3192b372baf3c22
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 21:01:31 +0200

    Fixed Betty Style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Using the printf function
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-      	printf("with proper grammar, but the outcome is a piece of art,\n\n");
-
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
