17494dbb4b8bd9d8e044708f24f7ee7f38a6d6cc
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 15:02:02 +0200

    Fixed Betty requirements

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int i = 0;
-  while (i < 10)
-  {
-    printf("%i", i);
-    i++;
-  }
-  putchar('\n');
-  return (0);
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%i". i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
