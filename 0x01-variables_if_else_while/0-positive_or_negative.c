fcbdc80523d97dd7d85630693f749df4fa8d1f24
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 12:23:24 +0200

    fixed all of betty errors/warnings FINALLYgit add 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -12,10 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	  printf("%i is positive\n", n);
+		printf("%i is positive\n", n);
 	else if (n == 0)
-	  printf("%i is zero\n", n);
+		printf("%i is zero\n", n);
 	else if (n < 0)
-	  printf("%i is negative\n", n);
+		printf("%i is negative\n", n);
 	return (0);
 }
