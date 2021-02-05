4a24d12216b52b2911069e9904fd113834e2796d
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 20:56:09 +0200

    Fixed Betty Style and formatted output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - puts character on screen
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-     	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
