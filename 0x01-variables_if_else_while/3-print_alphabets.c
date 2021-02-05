1ca01e5110432a1f5c3de49e7f0e1cbfacb2c77f
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 13:12:11 +0200

    Added return statement and fixed Betty

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int x, y;
-  for (x = 'a'; x <= 'z'; x++)
-    {
-      putchar(x);
-    }
-  for (y = 'A'; y <= 'Z'; y++)
-    {
-      putchar(y);
-    }
-  putchar('\n');
+	int x, y;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (y = 'A'; y <= 'Z'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
 }
