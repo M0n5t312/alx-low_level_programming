f4150dc3effb3cbc0ad4961d5f18b6d9e977236c
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 21:12:13 +0200

    Fixed Betty Style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
-int main() 
+/**
+ * main - print size of c variables
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    int intType;
-    float floatType;
-    long long int lng;
-    long int lngint;
+	int intType;
+	char c;
+	float floatType;
+	long long int lng;
+	long int lngint;
 
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of long int: %zu bytes\n", sizeof(lngint);
-    printf("Size of long long int: %zu bytes\n", sizeof(lng));
-    printf("Size of float: %zu byte\n", sizeof(floatType));
-    
-    return 0;
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(lngint));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lng));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	return (0);
 }
