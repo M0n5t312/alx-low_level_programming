9298ca8dc6aef6d391c40c3364a70c2c49566a7c
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 15:15:42 +0200

    Fixed Betty Requirements

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int first = 'a';
-  int last;
-  for (last = 'z'; last >= first; last--)
-    putchar(last);
-  putchar ('\n');
-  return (0);
+	int first = 'a';
+	int last;
+
+	for (last = 'z'; last >= first; last--)
+		putchar(last);
+	putchar('\n');
+	return (0);
 }
