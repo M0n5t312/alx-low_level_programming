dbadc0d8067961d4d635c01ae508f4fe8d2e1b03
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-05 15:23:10 +0200

    Fixed Betty requirements

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-  int first_digit, first_letter;
-  int last_digit = 57;
-  int  last_letter = 102;
-  for (first_digit = 48; first_digit <= last_digit; first_digit++)
-    putchar (first_digit);
-  for (first_letter = 97; first_letter <= last_letter; first_letter++)
-    putchar (first_letter);
-  putchar ('\n');
-  return (0);
+	int first_digit, first_letter;
+	int last_digit = 57;
+	int last_letter = 102;
+
+	for (first_digit = 48; first_digit <= last_digit; first_digit++)
+		putchar(first_digit);
+	for (first_letter = 97; first_letter <= last_letter; first_letter++)
+		putchar(first_letter);
+	putchar('\n');
+	return (0);
 }
