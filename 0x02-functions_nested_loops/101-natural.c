256e2f1de2b356cc1ba1a78ba835460c0c6fdeb8
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-10 22:14:40 +0200

    excluded 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int result_3, result_5, count;
 
-	for (count = 0; count < 1024; count++)
+	for (count = 0; count < 1023; count++)
 	{
 		if (count % 3 == 0)
 			result_3 = result_3 + (count / 3);
 		if (count % 5 == 0)
 			result_5 = result_5 + (count / 5);
 	}
-	printf("Sum of multiples of 3 and 5 is : %i\n", result_3 + result_5);
+	printf("%i\n", result_3 + result_5);
 	return (0);
 }
