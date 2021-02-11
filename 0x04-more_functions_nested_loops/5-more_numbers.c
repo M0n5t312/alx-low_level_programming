135ee679498a1a8056e534296142d8ca7aade925
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-11 22:04:18 +0200

    fixed output

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,7 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j / 10 == 0)
-				continue;
-			else
+			if (j / 10 > 0)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
