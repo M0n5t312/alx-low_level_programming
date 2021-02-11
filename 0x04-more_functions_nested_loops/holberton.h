018c8d292b47a095f11ef7712f526edba59efcb0
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-11 21:02:21 +0200

    Header

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+int _putchar(char);
+void print_square(int);
+void print_triangle(int);
+#endif
