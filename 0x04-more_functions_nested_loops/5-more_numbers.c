af59fc1970da51cbfa0997a833cad2d5824aa4d7
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-11 21:10:20 +0200

    print 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times the numbers 0 - 14
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j / 10 == 0)
+				continue;
+			else
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
