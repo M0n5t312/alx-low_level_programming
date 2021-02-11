637d960ae4cddeac5db95b6e882c9f3a4c588680
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-11 21:53:18 +0200

    fixed space in output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%i ", i);
+
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 
