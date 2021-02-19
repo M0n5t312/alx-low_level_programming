1ba68032444ba0185e6ea871b11a631bc8388569
Menzi Khumalo <khumalomnzi@gmail.com>
2021-02-19 20:48:52 +0200

    Fixed output

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i;
 
-	while (src[i] != '\0')
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
 	{
-		if (i < n)
-		{
-			dest[j] = src[i];
-		}
+		dest[i] = '\0';
 		i++;
-		j++;
 	}
-	dest[j] = '\0';
+
 	return (dest);
 }
