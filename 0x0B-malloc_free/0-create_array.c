d2e2faa3438cf9d29e7d8de6070d373fe2ca32a8
Menzi Khumalo <khumalomnzi@gmail.com>
2021-03-02 22:47:53 +0200

    fixed betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return NULL;
-	else
+		return (NULL);
+	while (i < size)
 	{
-		while (i < size)
-		{
-			*(ar + i) = c;
-			i++;
-		}
-		return ar;
+		*(ar + i) = c;
+		i++;
 	}
+	return (ar);
 }
